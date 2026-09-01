cc00a0c5d3011091e62391be7b14c4c2e38ecb99
lhancepedrozo <lhancepedrozo@gmail.com>
2026-09-01 20:20:58 +0800

    Initial commit of my java class

---
+++ src/UserInfo.java
@@ -0,0 +1,19 @@
+public class UserInfo {
+
+    public static void main(String[] args) {
+        String firstName = "Lhance Gerald", lastName = "Pedrozo",
+                address = "Pozorrubio", mobileNumber = "09634005146";
+        int age = 18;
+        double gwa = 98.23;
+
+        System.out.println(
+                "Firstname: " + firstName + "\n" +
+                "Lastname: " + lastName + "\n" +
+                "Address: " + address + "\n" +
+                "Mobile Number: " + mobileNumber + "\n" +
+                "Age: " + age + "\n" +
+                "Gwa: " + gwa );
+
+    }
+}
+
